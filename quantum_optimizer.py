0221fa96c4dd52d1eb899a4fbaa83ce534306512
cser-sanidhya <cser.sanidhya@gmail.com>
2026-09-07 20:14:20 +0530

    Update quantum_optimizer.py

--- quantum_optimizer.py
+++ quantum_optimizer.py
@@ -14,7 +14,7 @@ def quantum_risk_optimizer(risk_score):
     # Encode risk into qubit
     qc.ry(angle, 0)
 
-    # Measure
+    # Measure the Probability
     qc.measure(0, 0)
 
     simulator = AerSimulator()
